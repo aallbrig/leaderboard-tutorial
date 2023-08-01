1fb7159894a85614eb7db137622e55ce286ebbbc
Andrew Allbright <aallbrig@gmail.com>
2023-08-01 19:12:50 -0400

    Aurora serverless v1 config

--- infrastructure/tf/main.tf
+++ infrastructure/tf/main.tf
@@ -1,5 +1,18 @@
-# run a cloudformation stack
+data "aws_vpc" "default" {
+  default = true
+}
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 resource "aws_cloudformation_stack" "stack" {
   name = "leaderboard-stack"
   template_body = file("../cf/stack.yaml")
+  parameters = {
+    VpcId = data.aws_vpc.default.id
+    SubnetIds = join(",", data.aws_subnets.default.ids)
+  }
 }
